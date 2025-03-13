85a92b87b522737e9ad768364bf0e20e9055ea86
Tuomas <tuomaslin@gmail.com>
2025-03-13 14:50:50 +0200

    Add colors

--- install_tools.sh
+++ install_tools.sh
@@ -19,38 +19,38 @@ fi
 
 apt -y update
 
-echo "[*] Install tools:"
+echo -e "\e[31;1m[*] Install tools:\e[0m"
 apt -y install nmap wget git sqlmap gobuster ffuf rsync python3-pip gimp jq binwalk \
 openvpn python3-requests tree steghide exiftool foremost
 
-echo "[*] Sublime installation"
-echo "[*] Add GPG key:"
+echo -e "\e[31;1m[*] Sublime installation\e[0m"
+echo -e "\e[31;1m[*] Add GPG key:\e[0m"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 
-echo "[*] Add repository:"
+echo -e "\e[31;1m[*] Add repository:\e[0m"
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-echo "[*] Update"
+echo -e "\e[31;1m[*] Update\e[0m"
 apt-get update
 
-echo "[*] Install sublime"
+echo -e "\e[31;1m[*] Install sublime\e[0m"
 apt-get install sublime-text
 
-echo "[*] Modifying .bashrc"
-run_as_user tee -a /home/$NORMAL_USER/.bashrc <<EOF
+echo -e "\e[31;1m[*] Modifying .bashrc\e[0m"
+run_as_user tee -a "/home/$NORMAL_USER/.bashrc" <<EOF
 alias grep="grep --color=auto"
 EOF
 
-echo "[*] Edit color scheme"
+echo -e "\e[31;1m[*] Edit color scheme\e[0m"
 run_as_user gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 
-echo "[*] Changing DIR colors"
-run_as_user tee -a /home/$NORMAL_USER/.dircolors <<EOF
+echo -e "\e[31;1m[*] Changing DIR colors\e[0m"
+run_as_user tee -a "/home/$NORMAL_USER/.dircolors" <<EOF
 DIR 01;94
 EOF
 
-echo "[*] Modifying .nanorc"
-run_as_user tee -a /home/$NORMAL_USER/.nanorc <<EOF
+echo -e "\e[31;1m[*] Modifying .nanorc\e[0m"
+run_as_user tee -a "/home/$NORMAL_USER/.nanorc" <<EOF
 include /usr/share/nano/*.nanorc
 
 set tabsize 4
@@ -67,8 +67,8 @@ set fill 139
 bind ^G comment main
 EOF
 
-echo "[*] Modifying .tmux.conf"
-run_as_user tee -a /home/$NORMAL_USER/.tmux.conf <<EOF
+echo -e "\e[31;1m[*] Modifying .tmux.conf\e[0m"
+run_as_user tee -a "/home/$NORMAL_USER/.tmux.conf" <<EOF
 
 set -g base-index 1
 setw -g pane-base-index 1
@@ -78,9 +78,9 @@ set -g renumber-windows on
 set -g set-titles on
 EOF
 
-echo "[*] Set git information:"
+echo -e "\e[31;1m[*] Set git information:\e[0m"
 read -p "Give git username: " git_username
 read -p "Give git email: " git_email
 
-run_as_user git config --global user.name $git_username
-run_as_user git config --global user.email $git_email
+run_as_user git config --global user.name "$git_username"
+run_as_user git config --global user.email "$git_email"
